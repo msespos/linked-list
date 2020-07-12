4cb5caa3950903aaff21cf83d5e927796f3c89d6
Mike Esposito <mikeesposito6852@gmail.com>
2020-07-12 11:23:32 -0700

    Style changes based on Rubocop

--- main.rb
+++ main.rb
@@ -1,16 +1,15 @@
+# defines a node in the linked list
 class Node
-
   attr_accessor :value, :next
 
   def initialize
     @value = nil
     @next = nil
   end
-
 end
 
+# defines the linked list and its methods
 class LinkedList
-
   def initialize
     @head = Node.new
   end
@@ -20,7 +19,7 @@ class LinkedList
     node = Node.new
     node.value = value
     node.next = nil
-    @head.next == nil ? @head.next = node : tail.next = node
+    @head.next.nil? ? @head.next = node : tail.next = node
   end
 
   # adds a new node containing value to the start of the list
@@ -33,10 +32,11 @@ class LinkedList
 
   # returns the total number of nodes in the list
   def size
-    return 0 if @head.next == nil
+    return 0 if @head.next.nil?
+
     count = 1
     node = @head.next
-    until node.next == nil
+    until node.next.nil?
       node = node.next
       count += 1
     end
@@ -50,15 +50,17 @@ class LinkedList
 
   # tail returns the last node in the list
   def tail
-    return nil if @head.next == nil
+    return nil if @head.next.nil?
+
     node = @head.next
-    node = node.next until node.next == nil
+    node = node.next until node.next.nil?
     node
   end
 
   # returns the node at the given index
   def at(index)
-    return nil if @head.next == nil || index >= size || index < 0
+    return nil if @head.next.nil? || index >= size || index.negative?
+
     position = 0
     node = @head.next
     until position == index
@@ -70,26 +72,29 @@ class LinkedList
 
   # removes the last element from the list
   def pop
-    return nil if @head.next == nil
+    return nil if @head.next.nil?
+
     node = @head.next
-    node = node.next until node.next.next == nil
+    node = node.next until node.next.next.nil?
     node.next = nil
   end
 
   # returns true if the passed in value is in the list and otherwise returns false.
   def contains?(value)
-    return false if @head.next == nil
+    return false if @head.next.nil?
+
     node = @head.next
-    node = node.next until node.value == value || node.next == nil
+    node = node.next until node.value == value || node.next.nil?
     node.value == value
   end
 
   # returns the index of the node containing value, or nil if not found.
   def find(value)
-    return nil if @head.next == nil
+    return nil if @head.next.nil?
+
     index = 0
     node = @head.next
-    until node.value == value || node.next == nil
+    until node.value == value || node.next.nil?
       node = node.next
       index += 1
     end
@@ -97,12 +102,13 @@ class LinkedList
   end
 
   # represent your LinkedList objects as strings, so you can print them out and preview them
-    # in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> nil
+  # in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> nil
   def to_s
-    return "nil" if @head.next == nil
+    return "nil" if @head.next.nil?
+
     linked_list = "( #{@head.next.value} ) -> "
     node = @head.next
-    until node.next == nil
+    until node.next.nil?
       node = node.next
       linked_list += "( #{node.value} ) -> "
     end
@@ -124,9 +130,9 @@ class LinkedList
 
   # inserts the node with the provided value at the given index
   def insert_at(value, index)
-    if index > size || index < 0
+    if index > size || index.negative?
       nil
-    elsif index == 0 || @head.next == nil
+    elsif index.zero? || @head.next.nil?
       prepend(value)
     elsif index == size
       append(value)
@@ -141,9 +147,9 @@ class LinkedList
 
   # removes the node at the given index.
   def remove_at(index)
-    if @head.next == nil || index > size || index < 0
+    if @head.next.nil? || index > size || index.negative?
       nil
-    elsif index == 0
+    elsif index.zero?
       @head.next = @head.next.next
     elsif index == size
       pop
@@ -152,5 +158,4 @@ class LinkedList
       node.next = node.next.next
     end
   end
-
 end
